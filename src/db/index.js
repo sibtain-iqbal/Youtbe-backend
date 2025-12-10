895fc5a2984c28c795fa0a1c6f547ee7b60b2352
Sibtain-Iqbal <sibtainiqbal8844@gmail.com>
2025-12-10 14:33:12 +0500

    Connect to mongo db atlas

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+import express from "express";
+
+const app = express();
+
+const connectDB = () => {
+  try {
+    mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log("Connected Succesfully !!!");
+    app.listen(process.env.PORsT, () => {
+      console.log(`Your port number : ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(Error);
+    throw error;
+  }
+};
+
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import express from "express";
+import { DB_NAME } from "./constant.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config()
+
+connectDB()
+
+// const app = express()
+
+
+
+// const connectDB = async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         console.log("data base is succesfully connected");
+        
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`your port number  : ${process.env.PORT} and data base name is : ${DB_NAME}`)
+//         })
+
+//     }
+//     catch(error){
+
+//         console.error("your error is :",error)
+
+//     }
+// }
+
+// connectDB()
+
+
+
+// const app =  express()
+
+
+// dotenv.config()
+
+// ;(async()=>{
+
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         console.log("database is succesfully connected");
+
+//         app.listen(process.env.PORT , ()=>{
+//             console.log(`your port number is  ${process.env.PORT }`);
+         
+//         } )
+        
+        
+//     } catch (error) {
+//         console.error("ERROR IS : ")
+        
+//     }
+
+// })()
